8aa440e6dc4fe15e481c1a17827f9853a53660fe
TomSWD <78202370+TomSWD@users.noreply.github.com>
2022-09-16 08:29:46 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <MenuSection />
-  <!-- <MainSection />
+  <MainSection />
   <PriceList />
   <CooperationSection />
   <BrandsSection />
   <AboutSection />
   <ContactSection />
-  <FooterSection /> -->
+  <FooterSection />
 </template>
 
 <script>
@@ -14,25 +14,25 @@ import "@/assets/styles/reset.css";
 import "@/assets/styles/global.css";
 
 import MenuSection from "./components/MenuSection.vue";
-// import MainSection from "./components/MainSection.vue";
-// import PriceList from "./components/PriceList.vue";
-// import CooperationSection from "./components/CooperationSection.vue";
-// import BrandsSection from "./components/BrandsSection.vue";
-// import AboutSection from "./components/AboutSection.vue";
-// import ContactSection from "./components/ContactSection.vue";
-// import FooterSection from "./components/FooterSection.vue";
+import MainSection from "./components/MainSection.vue";
+import PriceList from "./components/PriceList.vue";
+import CooperationSection from "./components/CooperationSection.vue";
+import BrandsSection from "./components/BrandsSection.vue";
+import AboutSection from "./components/AboutSection.vue";
+import ContactSection from "./components/ContactSection.vue";
+import FooterSection from "./components/FooterSection.vue";
 
 export default {
   name: "App",
   components: {
     MenuSection,
-    // MainSection,
-    // PriceList,
-    // CooperationSection,
-    // BrandsSection,
-    // AboutSection,
-    // ContactSection,
-    // FooterSection,
+    MainSection,
+    PriceList,
+    CooperationSection,
+    BrandsSection,
+    AboutSection,
+    ContactSection,
+    FooterSection,
   },
 };
 </script>
